dc2e1c4dc88df8bfdcdf37475fe04ca6addd8c55
pedroabreuu <99131505+pedroabreuu@users.noreply.github.com>
2026-03-21 10:44:35 -0300

    Update: adicionando NN no main.cpp

--- cvrp/src/main.cpp
+++ cvrp/src/main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include "parser.h"
+#include "nearestNeighbor.h"
 
 int main(int argc, char* argv[]) {
     std::string filepath = (argc > 1) ? argv[1] : "data/A-n32-k5.vrp";
 
     try {
         VRPInstance instance = parseVRP(filepath);
+        Solution solucaoNN = nearestN(instance);
+        solucaoNN.calculaCusto(instance);
 
         std::cout << "Instância carregada.\n";
         std::cout << "Depot ID:     " << instance.depot_id   << "\n";
         std::cout << "Capacidade:   " << instance.capacity   << "\n";
         std::cout << "Nós:          " << instance.nodes.size() << "\n";
         std::cout << "Caminhões:    " << instance.num_trucks  << "\n";
+        solucaoNN.imprime(instance);
     }
     catch (const std::exception& e) {
         std::cerr << "Erro: " << e.what() << "\n";
