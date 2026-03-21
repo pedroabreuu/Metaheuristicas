498945123d9da07a44bdfc4a03cb05b0b5000e53
pedroabreuu <99131505+pedroabreuu@users.noreply.github.com>
2026-03-21 08:11:09 -0300

    feat: adding Solution.h e Soltion.cpp

---
+++ cvrp/include/Solution.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <vector> 
+#include <VRPInstance.h>
+
+struct Solution { 
+  std::vector<std::vector<int>> rotas;
+  int custoTotal = 0;
+
+  void calculaCusto(const VRPInstance& instance);
+  void imprime(const VRPInstance& instance) const;
+};
---
+++ cvrp/src/Solution.cpp
@@ -0,0 +1,42 @@
+#include <Solution.h> 
+#include <iostream>
+
+void Solution::calculaCusto(const VRPInstance& instance) {
+  custoTotal = 0;
+  const Node& depot = instance.getDepot();
+
+  for (const auto& rota: rotas) {
+    // comeca no depot
+    const Node* anterior = &depot;
+    for (int id: rota) { // para cada node encontrado, acumula a distancia
+      for (const auto& node: instance.nodes) {
+        if (node.id == id) {
+          custoTotal += instance.distancia(*anterior, node);
+          anterior = &node;
+          break;
+        }
+      }
+    }
+    // voltando pro deposito
+    custoTotal += instance.distancia(*anterior, depot);
+  }
+}
+
+void Solution::imprime(const VRPInstance& instance) const {
+  int depot = instance.depot_id;
+  
+  for (size_t i = 0; i < rotas.size(); i++) {
+    std::cout << "Rota: " << i + 1 << "\n";
+    std::cout << depot;
+
+    for (size_t j = 0; j < rotas[i].size(); j++) {
+      std::cout << "->" <<rotas[i][j];  
+    }
+
+    std::cout << "->" << depot << std::endl;
+  }
+
+  std::cout << "Custo final: " << custoTotal << std::endl;
+}
+
+
