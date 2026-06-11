518ad8cd9efb2b36ab497a289ce92d8bc0a93fcf
pedroabreuu <99131505+pedroabreuu@users.noreply.github.com>
2026-06-11 14:04:01 -0300

    fix: adicionando tempo limite no GA e BRKGA

--- cvrp/include/metaheuristicas/ga.h
+++ cvrp/include/metaheuristicas/ga.h
@@ -3,4 +3,4 @@
 #include "core/VRPInstance.h"
 #include "core/Solution.h"
 
-Solution GeneticAlgorithm(const VRPInstance& instance, int numGeracoes, int tamanhoPopulacao, int tamanhoTorneio, int elitismo, double probMutacao, double probCrossover);
+Solution GeneticAlgorithm(const VRPInstance& instance, int numGeracoes, int tamanhoPopulacao, int tamanhoTorneio, int elitismo, double probMutacao, double probCrossover, double tempoLimite = 0.0);
